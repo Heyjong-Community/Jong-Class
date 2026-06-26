a72a43348ff29261c766ce5cccef965aa31dac0f
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2026-06-26 22:28:37 +0700

    add(backend): service user and clearing controller user

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findByUsername(username: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findById(id: string) {
+    return this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findByGoogleId(googleId: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        googleId,
+      },
+    });
   }
 }
